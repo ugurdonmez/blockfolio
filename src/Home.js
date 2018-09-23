abaf290723c46f4c02ebb737fdabb96292ee9957
Ugur Donmez <ugurdonmez87@gmail.com>
2018-09-23 22:27:26 +0200

    init

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+import { Header } from "react-native-elements";
+
+class Home extends Component {
+    render() {
+        return (
+            <View>
+                <Header>
+                    
+                </Header>
+
+                <Text> ugur </Text>
+            </View>
+        );
+    }
+}
+
+export default Home;
